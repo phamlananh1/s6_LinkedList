802aaa612c85c98297205c1c85aed38aa8d27db4
Lan Anh <lananh@MacBook-Pro-cua-Lan.local>
2019-02-18 20:33:48 +0700

    file

---
+++ LinkList.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lananh
+ * Date: 2019-01-29
+ * Time: 13:39
+ */
+include_once "Node.php";
+
+class LinkList
+{
+    private $firstNode;
+    private $lastNode;
+    private $count;
+
+    function __construct()
+    {
+        $this->firstNode = NULL;
+        $this->lastNode = NULL;
+        $this->count = 0;
+    }
+    public function insertFirst($data)
+    {
+        $link = new Node($data);
+        $link->next = $this->firstNode;
+        $this->firstNode = $link;
+
+        if ($this->firstNode == NULL)
+            $this->lastNode = $link;
+
+        $this->count++;
+    }
+    public function insertLast($data)
+    {
+        if ($this->firstNode != NULL)
+        {
+            $link = new Node($data);
+            $this->lastNode->next = $link;
+            $link->next = NULL;
+            $this->lastNode = $link;
+            $this->count++;
+        }
+        else
+        {
+            $this->insertFirst($data);
+        }
+    }
+    public function totalNodes()
+    {
+        return $this->count;
+    }
+    public function readList()
+    {
+        $listData = array();
+        $current = $this->firstNode;
+
+        while($current != NULL)
+        {
+            array_push($listData, $current->readNode());
+            $current = $current->next;
+        }
+        return $listData;
+    }
+}
